576a73cbef0f494dc58e0f8f1e6082c5c5821113
Ingkamon Poolnual <ingkamon16764daivo@gmail.com>
2022-02-07 22:24:27 +0700

    update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 
-class UsersSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
--- routes/api.php
+++ routes/api.php
@@ -16,14 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function(){
-    Route::post('logout',[AuthController::class, 'logout']);
-    Route::resource('products',ProductController::class);
-} );
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::resource('products', ProductController::class);
+});
 
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+    
